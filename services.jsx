fa5fe6475706db53dbdbed80b752010cabfde102
KamoEllen <108501662+KamoEllen@users.noreply.github.com>
2022-12-11 13:40:56 +0200

    Create services.jsx

---
+++ services.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Services = () => {
+  return (
+    <div>
+      <h2>Services</h2>
+      <p>I offer the following services:</p>
+      <ul>
+        <li>Web design and development</li>
+        <li>Graphic design</li>
+        <li>Content creation</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Services;
