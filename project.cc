13e2cc7cc76f131821bfba601ab5685bd9f17e34
KamoEllen <108501662+KamoEllen@users.noreply.github.com>
2022-12-11 13:41:53 +0200

    Create project.cc

---
+++ project.cc
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Projects = () => {
+  return (
+    <div>
+      <h2>Projects</h2>
+      <p>Here are some of my past projects:</p>
+      <ul>
+        <li>
+          <h3>Web design for XYZ Company</h3>
+          <p>I designed and developed a new website for XYZ Company, a leading provider of business services. The website features a clean and modern design, easy navigation, and responsive layout. It also includes a custom CMS for easy content management.</p>
+          <a href="https://xyzcompany.com" target="_blank" rel="noopener noreferrer">View website</a>
+        </li>
+        <li>
+          <h3>Graphic design for ABC Corporation</h3>
+          <p>I created a new brand identity for ABC Corporation, a global tech company. The new design includes a refreshed logo, color palette, and typography, as well as a set of marketing materials such as business cards, brochures, and social media graphics.</p>
+          <a href="https://abccorporation.com" target="_blank" rel="noopener noreferrer">View case study</a>
+        </li>
+        <li>
+          <h3>Blog design and content creation</h3>
+          <p>I redesigned and launched a new blog for a personal development coach. The new design features a clean and minimalist aesthetic, easy navigation, and a responsive layout. I also wrote and published a series of blog posts on topics related to personal growth and self-improvement.</p>
+          <a href="https://coach.com/blog" target="_blank" rel="noopener noreferrer">View blog</a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Projects;
